e0ced9c5e2cdc9a47244aafdc2d34acd0d27029d
ccandy <ccunandy2001@gmail.com>
2021-01-17 19:55:56 -0800

    add bloom effect

--- Assets/Bloom/Scrips/BloomScripts.cs
+++ Assets/Bloom/Scrips/BloomScripts.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class BloomScripts : MonoBehaviour
 {
     private int _width, _height;
+    private RenderTextureFormat _format;
+    [Range(1, 16)]
+    public int iterations = 1;
+
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         _width = source.width;
         _height = source.height;
+        _format = source.format;
+        RenderTexture _currentSource = source;
 
-        _width /= 2;
-        _height /= 2;
-        RenderTexture rt = RenderTexture.GetTemporary(_width, _height, 0, source.format);
+        for(int n = 0; n < iterations; n++) 
+        {
 
-        Graphics.Blit(source, rt);
-        Graphics.Blit(rt, destination);
+            _width /= 2;
+            _height /= 2;
+            if (_height / 2 < 0)
+            {
+                break;
+            }
 
-        RenderTexture.ReleaseTemporary(rt);
-
-
+            RenderTexture _currentDes = RenderTexture.GetTemporary(_width, _height, 0, _format);
+            Graphics.Blit(_currentSource, _currentDes);
+            _currentSource = _currentDes;
+            RenderTexture.ReleaseTemporary(_currentDes);
+        }
+        Graphics.Blit(_currentSource, destination);
+        RenderTexture.ReleaseTemporary(_currentSource);
     }
 }
