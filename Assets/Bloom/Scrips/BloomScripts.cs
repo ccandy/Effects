dcc97311327c32d58e44018fb261d90b52e582a5
ccandy <ccunandy2001@gmail.com>
2021-01-17 19:26:27 -0800

    setup bloom scene

--- Assets/Bloom/Scrips/BloomScripts.cs
+++ Assets/Bloom/Scrips/BloomScripts.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class BloomScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _width, _height;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        
-    }
+        _width = source.width;
+        _height = source.height;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _width /= 2;
+        _height /= 2;
+        RenderTexture rt = RenderTexture.GetTemporary(_width, _height, 0, source.format);
+
+        Graphics.Blit(source, rt);
+        Graphics.Blit(rt, destination);
+
+        RenderTexture.ReleaseTemporary(rt);
+
+
     }
 }
